4bec88e884708f7c00c46c5abda511b316e4df49
Hervé Audren <h.audren@gmail.com>
2015-06-22 15:50:37 +0900

    Just change the example

--- stability/example.py
+++ stability/example.py
@@ -1,17 +1,11 @@
 import numpy as np
 import stability as stab
 
-#normals = [np.array([[0, 0, 1]]).T]*3
 
 #normals = map(stab.normalize,
 #              [np.array([[0, 0, 1]]).T,
 #               np.array([[0, 0, 1]]).T,
 #               np.array([[1, 0, 0]]).T])
-#
-
-#pos = [np.array([[0.1, 0, 0]]).T,
-#       np.array([[-0.1, 0, 0]]).T,
-#       np.array([[0.2, 0, 1]]).T]
 #
 
 def main():
@@ -24,21 +18,27 @@ def main():
   #       np.array([[0, 1, 0]]).T,
   #       np.array([[1, 0, 0.2]]).T]
 
-  normals = map(stab.normalize,
-                [np.array([[0., 0., 1.]]).T,
-                 np.array([[0., 0., 1.]]).T,
-                 np.array([[0., 0., 1.]]).T,
-                 np.array([[0., 0., 1.]]).T])
+  #normals = map(stab.normalize,
+  #              [np.array([[0., 0., 1.]]).T,
+  #               np.array([[0., 0., 1.]]).T,
+  #               np.array([[0., 0., 1.]]).T,
+  #               np.array([[0., 0., 1.]]).T])
 
-  pos = [np.array([[0.1, 0.155, -0.105]]).T,
-         np.array([[-0.07, 0.155, -0.105]]).T,
-         np.array([[-0.07, 0.055, -0.105]]).T,
-         np.array([[0.1, 0.055, -0.105]]).T]
+  #pos = [np.array([[0.1, 0.155, -0.105]]).T,
+  #       np.array([[-0.07, 0.155, -0.105]]).T,
+  #       np.array([[-0.07, 0.055, -0.105]]).T,
+  #       np.array([[0.1, 0.055, -0.105]]).T]
+
+  normals = [np.array([[0, 0, 1]]).T]*3
+
+  pos = [np.array([[0.1, 0, 0]]).T,
+         np.array([[-0.1, 0, 0]]).T,
+         np.array([[0.2, 0, 1]]).T]
 
   bar = sum(pos)/float(len(pos))
   pos = [p-bar for p in pos]
 
-  mu = 0.7
+  mu = 0.5
   contacts = [stab.Contact(mu, p, n) for p, n in zip(pos, normals)]
 
   poly = stab.StabilityPolygon(60)
