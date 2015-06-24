471f59bc5b0de14fafd1bdb8fca6f29351e901bb
Hervé Audren <h.audren@gmail.com>
2015-06-24 13:02:00 +0900

    Disable Axes3D warning in flake8

--- stability/stability.py
+++ stability/stability.py
@@ -4,7 +4,7 @@ import cdd
 import shapely.geometry as geom
 
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D # flake8: noqa
+from mpl_toolkits.mplot3d import Axes3D  # noqa
 
 from scipy.linalg import block_diag
 
